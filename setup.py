3ea0d9938585809374578f5ab01bd75314eb2637
randy <randydu@gmail.com>
2020-05-01 11:37:57 -0700

    automate setup parameters;

--- setup.py
+++ setup.py
@@ -1,17 +1,42 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+author = "randydu"
+package_name = "beebird"
+packages = find_packages()
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name="beebird", 
-    version="0.0.1",
+def extract_version(filename):
+    import re
+    pattern = re.compile(r'''__version__\s*=\s*"(?P<ver>[0-9\.]+)".*''')
+    with open(filename, 'r') as fd:
+        for line in fd:
+            match = pattern.match(line)
+            if match:
+                ver = match.groupdict()['ver']
+                break
+        else:
+            raise Exception('ERROR: cannot find version string.')
+    return ver
+
+version = extract_version('%s/__init__.py' % package_name)
+
+url = "https://github.com/%s/%s.git" % (author, package_name)
+download_url = "https://github.com/%s/%s/archive/v%s.tar.gz" % (author, package_name, version)
+
+setup(name=package_name, 
+    packages=packages,
+    version=version,
     description = "task running platform for python 3",
     long_description = long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/randydu/beebird.git",
-    author="Randy Du",
-    author_email="randydu@gmail.com",
-    packages=["beebird"],
+    url=url,
+    download_url = download_url,
+    author = author,
+    author_email= "randydu@gmail.com",
     keywords=["task", "cmdlet"],
     license="MIT",
     classifiers=[
@@ -23,5 +48,4 @@ setup(name="beebird",
     'License :: OSI Approved :: MIT License',  
     'Programming Language :: Python :: 3',
   ],
-
 )
